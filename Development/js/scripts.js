c728ea1bd173c6d1ba6001f2f32ab350da103dba
مراحب عدنان يوسف - 577 <marahebx001@gmail.com>
2020-06-27 21:39:59 +0300

    تم حق احلى استاذة

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -1,34 +1,68 @@
 // السؤال الأول
 // 1
+ const name="maraheb"; 
+
+
 
 // 2
+let age=15;
 
 // 3 and 4
+console.log("im "+ name + " and im "+ age+ ' years old');
+
 
 // 5 and 6
+let xage = 5*age;
+console.log(xage)
+console.log(5*age)
+
+
 
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // السؤال الثاني
 // 1
+var hobby = ["swimming", "coding"];
+
+
 
 // 2
-
+hobby.push(15)
+hobby[3]=15
+console.log(hobby)
+console.log(hobby.length)
 // 3
+hobby.pop()
 
 // 4
+let student = {
+  track:"web",
+  language:"Javascript", 
+
+}
 
 // 5
+student["TAs"] = ["nancy","moudhi","rahaf","hussain"]
+console.log(student)
 
 // 6
+console.log(student["track"])
+console.log(studenttrack)
 
 // 7
+console.log(student["TAs"])
+
 
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // البونص
 // 1
-
+student["SayHello"] = function() {console.log("مراحب")}
+console.log(student)
 // 2
+student["SayHello"]()
+student["SayHello"]()
+
+
 
 // console results => console.png
